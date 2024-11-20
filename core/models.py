dd1d1bc5cfe81f7a71482ff7162438ced8b0d6ed
allen080 <luanfelliipe8@gmail.com>
2024-11-20 02:43:41 -0300

    changing blog views

--- core/models.py
+++ core/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    completed = models.BooleanField(default=False)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Post has one or more tags and Tag has one or more posts
+    tags = models.ManyToManyField(Tag) 
+
+    def __str__(self):
+        return self.title
 
 class Tag(models.Model):
     # Model Tag (Dados: name)
--- core/views.py
+++ core/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
     #return render(request, "core/base.html")
-    return HttpResponse("oi")
+    return HttpResponse("<h1>Desafio de codificação - AG Sistemas</h1>")
+
+def viewBlog(request):
+    posts = Post.objects.all()
+    tags = Tag.objects.all()
+    return render(request, 'core/blog.html', {'posts': posts, 'tags': tags})
