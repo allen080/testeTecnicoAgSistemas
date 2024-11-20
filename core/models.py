59d435073c62a98fbccec396c8f0ebfc71d7da54
allen080 <luanfelliipe8@gmail.com>
2024-11-20 02:00:05 -0300

    adding Tag model and blog.html

--- core/models.py
+++ core/models.py
@@ -3,3 +3,10 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
+
+class Tag(models.Model):
+    # Model Tag (Dados: name)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
